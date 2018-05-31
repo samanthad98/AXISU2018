071228ea44505b27745ea2b8f9e5a4e4bcfe105b
yd246 <yd246@cornell.edu>
2018-05-31 10:18:12 +0800

    Initial commit

---
+++ email.py
@@ -0,0 +1,11 @@
+# This is a automated email collecter designed for summer intern
+
+# read email account and password from a txt file
+def get_email_account
+
+# read the unread emails fulfill certain condition
+def get_email_content
+
+# write to excel file
+def wrt_excel
+
